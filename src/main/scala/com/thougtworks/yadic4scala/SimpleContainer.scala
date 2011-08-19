d202f0973c61ceab9ba878d846c3361934d6adbf
Alex Harin <aharin@thoughtworks.com>
2011-08-19 23:23:48 +0100

    Removing warnings and deprication

--- src/main/scala/com/thougtworks/yadic4scala/SimpleContainer.scala
+++ src/main/scala/com/thougtworks/yadic4scala/SimpleContainer.scala
@@ -17,28 +17,32 @@ class SimpleContainer(missingHandler: (Class[_]) => Object) extends Container {
 
   def resolveType[A <: Object]( aClass:Class[A] ): A = resolve(aClass).asInstanceOf[A]
 
-  def add[C <: Object](concrete: Class[C]): Unit = add(concrete, () => createInstance(concrete) )
+  def add[C <: Object](concrete: Class[C]) {
+    add(concrete, () => createInstance(concrete))
+  }
             
-  def add[I <: Object, C <: I](interface: Class[I], concrete: Class[C]): Unit = add(interface, () => createInstance(concrete) )
+  def add[I <: Object, C <: I](interface: Class[I], concrete: Class[C]) {
+    add(interface, () => createInstance(concrete))
+  }
 
-  def add[A <: Object](aClass: Class[A], activator: () => A): Unit = {
+  def add[A <: Object](aClass: Class[A], activator: () => A) {
     activators.containsKey(aClass) match {
       case true => throw new ContainerException(aClass.getName + " already added to container")
       case false => activators.put(aClass, new LazyActivator(activator))
     }
   }
 
-  def decorate[A <: Object, B <: A](interface: Class[A], concrete: Class[B]): Unit = {
+  def decorate[A <: Object, B <: A](interface: Class[A], concrete: Class[B]) {
     val existing = activators.get(interface)
     activators.put(interface, new LazyActivator(() => createInstance(concrete, (aClass: Class[_]) => {
-      if(aClass.equals(interface)) existing.activate else resolve(aClass)
+      if(aClass.equals(interface)) existing.activate() else resolve(aClass)
     })))
   }
 
   def createInstance[T <: Object](aClass: Class[T]): T = createInstance(aClass, resolve(_))
 
   def createInstance[T <: Object](aClass: Class[T], resolver: (Class[_]) => Object ): T = {
-    val constructors = aClass.getConstructors.toList.sort(_.getParameterTypes.length > _.getParameterTypes.length)
+    val constructors = aClass.getConstructors.toList.sortWith(_.getParameterTypes.length > _.getParameterTypes.length)
     constructors.foreach( constructor => {
       try {
         val instances = constructor.getParameterTypes.map( resolver(_) )
